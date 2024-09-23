c3e9a203d2bf88224df03a922b08e7f58a127e37
Kevin Guo <61925541+keeeeeevinguo@users.noreply.github.com>
2024-09-22 20:12:01 -0400

    Add files via upload

---
+++ src/main/webapp/app/modules/home/home.tsx
@@ -0,0 +1,51 @@
+import './home.scss';
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Translate } from 'react-jhipster';
+import { Row, Col, Alert, CarouselIndicators, Carousel, CarouselItem, CarouselCaption, CarouselControl } from 'reactstrap';
+
+import { useAppSelector } from 'app/config/store';
+
+export const Home = () => {
+  const account = useAppSelector(state => state.authentication.account);
+
+  return (
+    <Row>
+      <Col md="3" className="pad">
+        <span className="hipster rounded" />
+      </Col>
+      <Col md="9">
+        <h2>
+          <Translate contentKey="home.title">Welcome to CPOJ!</Translate>
+        </h2>
+        <p className="lead">
+          <Translate contentKey="home.subtitle"></Translate>
+        </p>
+        {account?.login ? (
+          <div>
+            <Alert color="success">
+              <Translate contentKey="home.logged.message" interpolate={{ username: account.login }}>
+                You are logged in as user {account.login}.
+              </Translate>
+            </Alert>
+          </div>
+        ) : (
+          <div>
+            <Alert color="warning">
+              <Translate contentKey="global.messages.info.register.noaccount">You do not have an account yet?</Translate>&nbsp;
+              <Link to="/account/register" className="alert-link">
+                <Translate contentKey="global.messages.info.register.link">Register a new account</Translate>
+              </Link>
+            </Alert>
+          </div>
+        )}
+        <p>
+          <Translate contentKey="home.question">If you have any question on JHipster:</Translate>
+        </p>
+      </Col>
+    </Row>
+  );
+};
+
+export default Home;
